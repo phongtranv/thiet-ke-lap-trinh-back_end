32a418d1eeddb3374bd674be79ced25189a3ec66
phongtranv <tranvanphong08092005@gmail.com>
2025-04-22 22:53:42 +0700

    Add files via upload

---
+++ Lap1.1_Bai10/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Nhập một số: ");
+int number = int.Parse(Console.ReadLine());
+bool isPrime = true;
+
+for (int i = 2; i <= Math.Sqrt(number); i++)
+{
+    if (number % i == 0)
+    {
+        isPrime = false;
+        break;
+    }
+}
+
+if (isPrime && number > 1)
+    Console.WriteLine("Số này là số nguyên tố.");
+else
+    Console.WriteLine("Số này không phải là số nguyên tố.");
---
+++ Lap1.1_Bai9/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Nhập số nguyên dương: ");
+int n = int.Parse(Console.ReadLine());
+int result = 1;
+for (int i = 1; i <= n; i++)
+{
+    result *= i;
+}
+Console.WriteLine($"Giai thừa của {n} là: {result}");
